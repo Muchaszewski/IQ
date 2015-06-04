62925924bceb9539d6f78e0645c8facc3379d53a
Muchaszewski <99999deuter@gmail.com>
2015-06-04 16:04:33 +0200

    Added images to repo

---
+++ Assets/Editor/StatisticsManagerEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class StatisticsManagerEditor : Editor {
+
+    private ToolTipManager script;
+    void OnEnable()
+    {
+        script = (ToolTipManager)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+//        if (GUILayout.Button("Set preview values"))
+//        {
+//            var spot = CurrentGame.Instance.Spot;
+//            ClearTooltipEditor();
+//#pragma warning disable 618
+//            script.CreateLabels(RandomItemFactory.CreateItem(1, spot));
+//#pragma warning restore 618
+//        }
+//        if (GUILayout.Button("Clear tooltip"))
+//        {
+//            ClearTooltipEditor();
+//        }
+    }
+
+
+    void ClearTooltipEditor()
+    {
+        //script.SetWindowSize(381);
+        //foreach (var o in script.TootipObjects)
+        //{
+        //    DestroyImmediate(o);
+        //}
+        //script.TootipObjects = new List<GameObject>();
+    }
+}
--- Assets/StatisticsManager.cs
+++ Assets/StatisticsManager.cs
@@ -1,15 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using InventoryQuest.Components.Entities.Player;
+using InventoryQuest.Game;
 
-public class StatisticsManager : MonoBehaviour {
+public class StatisticsManager : UIManager {
+
+    //_____________________________________________________________________________________________________________
+
+    private RectTransform _rectTransform;
+    private Player _player;
+
+    //__________________________________________________MonoBehaviour______________________________________________
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    _rectTransform = new RectTransform();
+	    _player = CurrentGame.Instance.Player;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    //______________________________________________End MonoBehaviour End__________________________________________
+
+
+
+    //______________________________________________Public Methods_________________________________________________
+
+    /// <summary>
+    /// Create all labels for tooltip
+    /// </summary>
+    /// <param name="item"></param>
+    [Obsolete("Only for inspector use, and SetTooltip method")]
+    public void CreateLabels(InventoryQuest.Components.Items.Item item)
+    {
+#if UNITY_EDITOR
+        _rectTransform = GetComponent<RectTransform>();
+#endif
+        //---------------
+
+        //---------------
+    }
+
+
+    //__________________________________________End Public Methods End_____________________________________________
+
+    void CreateTitle()
+    {
+        
+    }
+
 }
