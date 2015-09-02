731863160be9a3a08833fbe5dcab0e7de4a645c0
Muchaszewski <99999deuter@gmail.com>
2015-09-02 23:29:51 +0200

    Added primitive floating text

---
+++ Assets/FloatingTextAnimation.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FloatingTextAnimation : MonoBehaviour
+{
+    public float TimeForAnimation = 1f;
+    public float TimeForAlphaFade = 0.5f;
+    public float TimeForTurn = 0.3f;
+    public float AnimationHeight = 100;
+    public float AnimationMaxWidth = 70;
+    public float AnimationMinWidth = 50;
+    public bool IsRandomizingSide = true;
+    //[Tooltip("If enemy is on the left side, floating text will float to the left, and vice versa. Middle will only float upwards")]
+    //public bool IsDependOnEntitySize = true;
+    public bool IsIncreasingInSize = true;
+    public float TimeToSizeUp = 0.4f;
+
+    private Text _text;
+    private RectTransform _rectTransform;
+    private Vector3 _initialSize;
+
+    private float _currentAnimationTime = 0f;
+    private float _currentWidthMod;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _text = GetComponent<Text>();
+        _initialSize = transform.localScale;
+        if (IsIncreasingInSize)
+        {
+            transform.localScale = Vector3.zero;
+        }
+        _currentWidthMod = Random.Range(AnimationMinWidth, AnimationMaxWidth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentAnimationTime += Time.deltaTime;
+        if (IsIncreasingInSize)
+        {
+            if (transform.localScale != _initialSize)
+            {
+                transform.AddScale(new Vector3((_initialSize.x / TimeToSizeUp) * _currentAnimationTime, (_initialSize.y / TimeToSizeUp) * _currentAnimationTime));
+                if (transform.localScale.x >= _initialSize.x)
+                {
+                    IsIncreasingInSize = false;
+                    transform.localScale = _initialSize;
+                }
+            }
+        }
+
+        transform.AddY(AnimationHeight / (TimeForAnimation * 100f));
+
+        if (_currentAnimationTime >= TimeForAlphaFade)
+        {
+            var alphaMod = (1 / (TimeForAnimation - TimeForAlphaFade)) * Time.deltaTime;
+            _text.color = new Color(_text.color.r, _text.color.g, _text.color.b, _text.color.a - alphaMod);
+        }
+        if (TimeForTurn <= _currentAnimationTime)
+        {
+            var mod = (_currentWidthMod / (TimeForAnimation - TimeForTurn)) * Time.deltaTime;
+            mod = IsRandomizingSide ? mod : TransformUtils.RandomBool() ? mod : -mod;
+            transform.AddX(mod);
+        }
+        if (TimeForAnimation <= _currentAnimationTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
